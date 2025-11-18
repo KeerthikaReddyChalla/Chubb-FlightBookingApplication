e9dd57f9c87f8214c78729da586a79ed8440f6f9
KeerthikaReddyChalla <149089773+KeerthikaReddyChalla@users.noreply.github.com>
2025-11-18 23:04:36 +0530

    resolved search route error

--- src/main/java/com/flightapp/service/FlightInventoryService.java
+++ src/main/java/com/flightapp/service/FlightInventoryService.java
@@ -1,8 +1,11 @@
 package com.flightapp.service;
 
+import java.util.*;
 import com.flightapp.dto.InventoryRequest;
 import com.flightapp.model.FlightInventory;
 
 public interface FlightInventoryService {
     FlightInventory addInventory(InventoryRequest request);
+    List<FlightInventory> searchFlights(String origin, String destination, String date);
+
 }
