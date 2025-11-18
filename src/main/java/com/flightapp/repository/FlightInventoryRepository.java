d7926e704ffc2fcfffe0011689e5061fa0c736b5
KeerthikaReddyChalla <149089773+KeerthikaReddyChalla@users.noreply.github.com>
2025-11-18 23:02:54 +0530

    resolved search route error

--- src/main/java/com/flightapp/repository/FlightInventoryRepository.java
+++ src/main/java/com/flightapp/repository/FlightInventoryRepository.java
@@ -1,8 +1,11 @@
 package com.flightapp.repository;
 
 import com.flightapp.model.FlightInventory;
+import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FlightInventoryRepository extends JpaRepository<FlightInventory, Long> {
     FlightInventory findByFlightNumber(String flightNumber);
+    List<FlightInventory> findByOriginAndDestination(String origin, String destination);
+
 }
